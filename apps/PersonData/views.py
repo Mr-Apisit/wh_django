90f14a8e5cbfe4d50a235e690dd64277ea9f6426
Mr.Tree(ton) <apisit_yu@rtaf.mi.th>
2022-06-25 07:28:54 +0700

    delete comment

--- apps/PersonData/views.py
+++ apps/PersonData/views.py
@@ -5,8 +5,5 @@ from .person_data import person_list
 # Create your views here.
 
 def read_person_data(request):
-    # list_name = []
-    # for person in person_list:
-    #     list_name.append(person["full_name"])
     context = {"persons" : person_list}
     return render(request,"list.html",context)
